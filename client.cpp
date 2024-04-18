f56c2614e0e4d8d2ebfe46716caaeb9634fc8c27
Badr El Gezeri <99209046+BadrElGezeri@users.noreply.github.com>
2024-04-18 04:20:33 +0200

    Added multiple user support
    Added users and rooms

--- client.cpp
+++ client.cpp
@@ -1,73 +1,77 @@
 #include <iostream>
-#include <string>
 #include <cstring>
-#include <netinet/in.h>
-#include <limits>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+#include <thread>
 
 using namespace std;
 
+string username;
+
+void receiveMessages(int socket) {
+    char message[1024] = {0};
+    int bytesReceived;
+    while ((bytesReceived = recv(socket, message, sizeof(message), 0)) > 0) {
+        cout << message << endl;
+        memset(message, 0, sizeof(message)); // Clear buffer for the next receive
+    }
+    if (bytesReceived == -1) {
+        cerr << "Receive failed" << endl;
+    }
+}
+
+void sendMessages(int socket) {
+    string message;
+    while (true) {
+        cout << "> ";
+        getline(cin, message);
+        if (message == "STOP") {
+            cout << "Exiting application" << endl;
+            cout << "Press Enter to close" << endl;
+            cin.get();
+            exit(0);
+        }
+        message = username + ": " + message;
+        send(socket, message.c_str(), message.size(), 0);
+    }
+}
+
 int main() {
-    // Creating socket
+    cout << "Enter your username: ";
+    getline(cin, username);
+
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (clientSocket == -1) {
         cerr << "Error in creating socket" << endl;
         return 1;
     }
 
-    // Specifying address
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr);
 
-    // Sending connection request
     if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
         cerr << "Connection failed" << endl;
         close(clientSocket);
         return 1;
     }
 
-    int userChoice;
-    char username[50]; // Assuming username won't exceed 99 characters
-    string message;     // Using std::string for message
+    // Send username to server
+    send(clientSocket, username.c_str(), username.size(), 0);
 
-    // Sending data
-    cout << "Welcome to SecureChat" << endl;
-    cout << "Pick one of the following options to proceed" << endl;
-    cout << R"(
-[1]- Connect to a chat room
-[2]- Exit
->)";
-    cin >> userChoice;
+    cout << "Enter room name: ";
+    string roomName;
+    getline(cin, roomName);
+    send(clientSocket, roomName.c_str(), roomName.size(), 0);
 
-    if (userChoice == 1) {
-        cout << "[]Enter a username:\n>";
-        cin >> username;
+    thread receiveThread(receiveMessages, clientSocket);
+    thread sendThread(sendMessages, clientSocket);
 
-        // Clearing the input buffer
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Connected to chat room, to exit type \"STOP\"" << endl;
-        cout << username << ": ";
-        getline(cin, message);
-        while(message != "STOP"){
-            // Concatenating username and message using std::string
-            string output = username;
-            output += ": " + message;
+    receiveThread.join();
+    sendThread.join();
 
-            // Sending the concatenated string
-            send(clientSocket, output.c_str(), output.length(), 0);
-            cout << username << ": ";
-            getline(cin, message);
-        }
-        exit(0);
-
-    } else if (userChoice == 2) {
-        exit(0);
-    }
-
-    // Closing socket
     close(clientSocket);
 
     return 0;
