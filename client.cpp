4e4816f34aa496005bbe80b23a55cac1aed02c9e
Badr El Gezeri <99209046+BadrElGezeri@users.noreply.github.com>
2024-04-22 02:15:12 +0200

    Update client.cpp
    Fixed issues with the hashing and rooms and improved overall program useability

--- client.cpp
+++ client.cpp
@@ -8,12 +8,66 @@
 using namespace std;
 
 string username;
+char userChoice;
+
+string encrypt(string text)
+{
+    string result = "";
+    int key = 4; // Encryption Key
+
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_=-+"; // Encryption Alphabet
+
+    // Traverse text
+    for (int i = 0; i < text.length(); i++) {
+        
+        size_t pos = alphabet.find(text[i]); // Find the position of the current character in the alphabet
+
+
+        if (pos != string::npos) { // Validation if the character is found within the alphabet
+            pos = (pos + key) % alphabet.length(); // Shift the character by the key
+            result += alphabet[pos]; // Append the encrypted character to the result
+        }
+        else {
+            result += text[i];// If the character is not found in the alphabet, keep it unchanged
+        }
+    }
+    return result; // Return the resulting string
+}
+
+string decrypt(string text)
+{
+    string result = "";
+    int key = 4;
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_=-+"; // String alphabet
+
+    // Traverse text
+    for (int i = 0; i < text.length(); i++) {
+        // Find the position of the current character in the alphabet
+        size_t pos = alphabet.find(text[i]);
+
+        // If the character is found in the alphabet
+        if (pos != string::npos) {
+ 
+            pos = (pos - key + alphabet.length()) % alphabet.length(); //  Shift the character by the key in the opposite direction (decrypt)
+            
+            result += alphabet[pos]; // Append the decrypted character to the result
+        }
+        else {
+            result += text[i]; // If the character is not found in the alphabet, keep it unchanged
+        }
+    }
+
+    // Return the resulting string
+    return result;
+}
 
 void receiveMessages(int socket) {
     char message[1024] = {0};
     int bytesReceived;
     while ((bytesReceived = recv(socket, message, sizeof(message), 0)) > 0) {
-        cout << message << endl;
+        //cout << "Encrypted Message: " << message << endl;
+        // cout << "Decrypted Message: " << decrypt(message) << endl;
+        cout << decrypt(message) << endl;
         memset(message, 0, sizeof(message)); // Clear buffer for the next receive
     }
     if (bytesReceived == -1) {
@@ -27,19 +81,30 @@ void sendMessages(int socket) {
         cout << "> ";
         getline(cin, message);
         if (message == "STOP") {
+            message = username + " has left the chat room.";
+            string encryptedMessage = encrypt(message);
+            send(socket, encryptedMessage.c_str(), encryptedMessage.size(), 0);
             cout << "Exiting application" << endl;
             cout << "Press Enter to close" << endl;
             cin.get();
             exit(0);
         }
         message = username + ": " + message;
-        send(socket, message.c_str(), message.size(), 0);
+        string encryptedMessage = encrypt(message);
+        send(socket, encryptedMessage.c_str(), encryptedMessage.size(), 0);
     }
 }
 
 int main() {
-    cout << "Enter your username: ";
-    getline(cin, username);
+            cout << "[]Pick one of the following options to proceed:" << endl;
+            cout << R"(
+[1]- Sign Up
+[2]- Login
+[3]- Exit Application
+)";
+
+    cin >> userChoice;
+    cin.ignore(); // Ignore the newline character left in the buffer
 
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (clientSocket == -1) {
@@ -58,21 +123,63 @@ int main() {
         return 1;
     }
 
-    // Send username to server
-    send(clientSocket, username.c_str(), username.size(), 0);
+    // Send user choice to server
+    send(clientSocket, &userChoice, sizeof(userChoice), 0);
 
-    cout << "Enter room name: ";
-    string roomName;
-    getline(cin, roomName);
-    send(clientSocket, roomName.c_str(), roomName.size(), 0);
+    if (userChoice == '1') {
+        cout << "Enter your username: ";
+        getline(cin, username);
+        cout << "Enter your password: ";
+        string password;
+        getline(cin, password);
+        //cin.ignore();
 
-    thread receiveThread(receiveMessages, clientSocket);
-    thread sendThread(sendMessages, clientSocket);
+        string userSignup = username + "|" + password + " ";
+        send(clientSocket, userSignup.c_str(), userSignup.size(), 0);
+    } else if (userChoice == '2') {
+        cout << "Enter your username: ";
+        getline(cin, username);
+        cout << "Enter your password: ";
+        string password;
+        getline(cin, password);
 
-    receiveThread.join();
-    sendThread.join();
+        string userLogin = username + "|" + password + " ";
+        send(clientSocket, userLogin.c_str(), userLogin.size(), 0);
+        char message[1024] = {0};
+        int bytesReceived;
+        while ((bytesReceived = recv(clientSocket, message, sizeof(message), 0)) > 0) {
+        string returnMessage = message;
+        cout << returnMessage << endl;
+        memset(message, 0, sizeof(message)); // Clear buffer for the next receive
+       if(returnMessage == "True"){
+        cout << "Enter room name: ";
+        string roomName;
+        cin >> roomName;
+        roomName = roomName + "|";
+        send(clientSocket, roomName.c_str(), roomName.size(), 0);
+        string JoinedMessage = username + " has joined the room.";
+        string encryptedJoinMessage = encrypt(JoinedMessage);
+        send(clientSocket, encryptedJoinMessage.c_str(), encryptedJoinMessage.size(), 0);
 
-    close(clientSocket);
+        thread receiveThread(receiveMessages, clientSocket);
+        thread sendThread(sendMessages, clientSocket);
+
+        receiveThread.join();
+        sendThread.join();
+
+        close(clientSocket);
+        }}
+    } else if (userChoice == '3'){
+        cout << "Exiting Application...";
+        cout.flush();
+        sleep(2);
+        close(clientSocket);
+        exit(1);
+        return 1;
+    }
+
+    
+
 
     return 0;
 }
